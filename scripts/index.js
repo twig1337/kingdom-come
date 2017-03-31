762f940dc51a27d571085bf9569d262657a0063b
Thomas Ellsworth <tellsworth@intellifarms.com>
2017-03-31 17:02:53 -0500

    Animations.

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,29 @@
 require('./../index.html');
 require('./../styles/main.scss');
 require('bootstrap');
+
+$(document).ready(function() {
+  var $animation_elements = $('.animated');
+  var $window = $(window);
+
+  function checkElementsVisible() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each($animation_elements, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+
+      //check to see if this current container is within viewport
+      if (element_bottom_position >= window_top_position && element_top_position <= window_bottom_position) {
+        $element.addClass('revealed');
+      }
+    });
+  }
+
+  $window.on('scroll resize', checkElementsVisible);
+  $window.trigger('scroll');
+});
