f123536da1ac59014aa59ecbadf1e2bdd21b3d61
Thomas Ellsworth <tellsworth@rxsavingssolutions.com>
2017-07-23 21:51:43 -0500

    Got animations working as desired.

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-/* global TimelineLite, Back, Power1 */
+/* global TimelineMax, Back, Power1 */
 
 import './../index.html';
 import './../styles/main.scss';
@@ -9,7 +9,7 @@ import $ from 'jquery';
 import ScrollSpy from './scroll-spy';
 
 const scrollSpy = new ScrollSpy();
-const introTimeline = new TimelineLite();
+const introTimeline = new TimelineMax();
 
 let quoteLineOne = $('#intro-quote-line-one');
 scrollSpy.add(quoteLineOne, () =>
@@ -21,19 +21,20 @@ scrollSpy.add(quoteLineOne, () =>
       ease: Power1.easeOut
     })
     .to($('#intro-quote-line-two'), 4, {
-      x: 25,
-      y: 5,
       opacity: 1,
       ease: Power1.easeOut
     }, '-=0.25')
-    .to($('#intro-quote-citation'), 3, {
-      y: 15,
+    .to($('#demon-highlight'), 2, {
+      scale: 1.05,
+      rotation: 900,
+      x: 1000,
+      y: -1000,
+      color: '#8A0707',
+      ease: Power1.easeIn
+    }, '-=2')
+    .to($('#intro-quote-citation'), 4, {
+      y: 10,
       opacity: 1,
       ease: Power1.easeOut
-    }, '-=2.5')
-    .to($('#demon-highlight'), 3, {
-      color: '#8A0707',
-      // scale: 1.5,
-      ease: Power1.easeOut
-    })
+    }, '+=1')
 );
